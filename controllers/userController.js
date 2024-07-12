018807429faabb11d3f7f443e2c74e4811067d0b
ChamodiLiyanage <it21828966@my.sliit.lk>
2024-07-12 15:48:04 +0530

    User location update controller and route

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,3 +22,25 @@ exports.addUser = async (req, res) => {
     res.status(500).send("Server error");
   }
 };
+exports.updateUserLocation = async (req, res) => {
+  const { email } = req.params;
+  const { location } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.location = location;
+    await user.save();
+
+    const weatherData = await getWeatherData(location);
+    user.weatherData.push({ date: new Date(), data: weatherData });
+    await user.save();
+
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addUser } = require("../controllers/userController");
+const {
+  addUser,
+  updateUserLocation,
+} = require("../controllers/userController");
 
 router.post("/", addUser);
+router.put("/:email", updateUserLocation);
 
 module.exports = router;
