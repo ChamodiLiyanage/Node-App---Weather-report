a5b3ed4ff725c59fa88917c4d30155dad9edd278
ChamodiLiyanage <it21828966@my.sliit.lk>
2024-07-12 14:09:14 +0530

    fetch weather data

---
+++ services/weatherService.js
@@ -0,0 +1,13 @@
+const axios = require("axios");
+
+exports.getWeatherData = async (location) => {
+  try {
+    const response = await axios.getAdapter(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHERMAP_API_KEY}`
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err.message);
+    throw err;
+  }
+};
