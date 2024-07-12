ba84c4dae62610d8ad40ff2706deba61a34b0dba
ChamodiLiyanage <it21828966@my.sliit.lk>
2024-07-12 14:40:00 +0530

    updated weatherService

--- services/weatherService.js
+++ services/weatherService.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.getWeatherData = async (location) => {
   try {
-    const response = await axios.getAdapter(
+    const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHERMAP_API_KEY}`
     );
     return response.data;
