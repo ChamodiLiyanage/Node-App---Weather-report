4e4efc58246dae8d0263d1bca6d074fdcadcc9a9
ChamodiLiyanage <it21828966@my.sliit.lk>
2024-07-12 16:21:32 +0530

    Weather Data for date

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,10 +35,35 @@ exports.updateUserLocation = async (req, res) => {
     await user.save();
 
     const weatherData = await getWeatherData(location);
-    user.weatherData = [{ date: new Date(), data: weatherData }];
+    user.weatherData.push({ date: new Date(), data: weatherData });
     await user.save();
 
     res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
+
+exports.getUserWeatherData = async (req, res) => {
+  const { email } = req.params;
+  const { date } = req.query;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const weatherDataForDate = user.weatherData.filter((data) => {
+      return new Date(data.date).toISOString().split("T")[0] === date;
+    });
+
+    if (weatherDataForDate.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No weather data found for this date" });
+    }
+    res.json(weatherDataForDate);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   addUser,
   updateUserLocation,
+  getUserWeatherData,
 } = require("../controllers/userController");
 
 router.post("/", addUser);
 router.put("/:email", updateUserLocation);
+router.get("/:email/weather", getUserWeatherData);
 
 module.exports = router;
