24aaad83aac26b57902f53198ce99db7a2b2fa04
ChamodiLiyanage <it21828966@my.sliit.lk>
2024-07-12 14:39:36 +0530

    add user route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const userRoute = require("./routes/userRoutes");
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +8,8 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+
+app.use("/api/users", userRoute);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
